a20e8457b08e125838fc42d7d88e54e4134b06c7
AJ-Romera <ajromeraalfonso@gmail.com>
2020-12-08 19:25:54 +0100

    Make API Create an API endpoint to return list of posts, set up a NodeJS server, and create post.js for interacting with data.json

---
+++ exercise/api/models/posts.js
@@ -0,0 +1,27 @@
+const PATH = "./data.json";
+const fs = require('fs');
+
+class Post {
+
+    get() {
+        /** Get Posts */
+        return this.readData();
+    }
+
+    getIndividualBlog() {
+        /** Get One Blog Posts */
+    }
+
+    add() {
+        /** Add new Post */
+    }
+
+    readData() {
+        let rawdata = fs.readFileSync(PATH);
+        let posts = JSON.parse(rawdata);
+        return posts;
+    }
+
+}
+
+module.exports = Post;
---
+++ exercise/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const Post = require("./api/models/posts");
+const postsData = new Post();
+
+app.get("/", (req, res) => {
+    res.status(200).send('This is working fine')
+});
+
+app.get("/api/posts", (req, res) => {
+    res.status(200).send(postsData.get());
+});
+
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
